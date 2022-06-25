a284e8a47a93d17ed7090874bae0fb7bee77732b
joseph-bravo <josephangelobravo@gmail.com>
2022-06-25 00:38:06 -0700

    integrated supabase oauth through twitter user data is stored within

--- src/lib/supabase-client.js
+++ src/lib/supabase-client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Supposedly, this is safe... lmao
-const supabaseUrl = `https://govahthyxiauwdtteylw.supabase.co`;
-const supabaseAnonKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvdmFodGh5eGlhdXdkdHRleWx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYxMTkzMTcsImV4cCI6MTk3MTY5NTMxN30.E8KwqGUuPkoTnn4THd4uV0tPSAYdmbdoNn0Iax3A1E4`;
+const supabaseUrl = `https://ukerwykabjfrwjecunfv.supabase.co`;
+const supabaseAnonKey = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVrZXJ3eWthYmpmcndqZWN1bmZ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTYxMzg5NjMsImV4cCI6MTk3MTcxNDk2M30.wJISIhbLD9Qj8qvN6OrVlvNBTPUZwtuo12zt8VWkptk`;
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,31 +1,13 @@
 <script>
 	// @ts-nocheck
 	import supabase from '$lib/supabase-client';
-
-	async function signInWithTwitter() {
-		const { user, session, error } = await supabase.auth.signIn({
-			provider: 'twitter'
-		});
-		console.log('user:', user);
-		console.log('session:', session);
-		console.log('error:', error);
-	}
-
-	async function signout() {
-		const { error } = await supabase.auth.signOut();
-		console.log('error:', error);
-	}
+	import user from '$stores/userSession';
+	import UserWidget from '$components/UserWidget.svelte';
 
 	supabase.auth.onAuthStateChange((event, session) => {
 		console.log('Auth State has Changed:', event, session);
+		$user = supabase.auth.user();
 	});
 </script>
 
-<button class="btn" on:click={signInWithTwitter}>Sign In</button>
-<button class="btn" on:click={() => console.log('user:', supabase.auth.user())}
-	>Log Current User</button
->
-<button class="btn" on:click={() => console.log('session:', supabase.auth.session())}
-	>Log Current Session</button
->
-<button class="btn" on:click={signout}>Sign Out</button>
+<UserWidget />
