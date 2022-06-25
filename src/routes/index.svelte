dd8e4e3dac8681b2a6686624c7c2d67f809c5bd0
joseph-bravo <josephangelobravo@gmail.com>
2022-06-25 12:50:40 -0700

    enabled downloads provided a post request given filename and renamed file todo: move to get request and query posts table for preferred file name

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,3 +11,31 @@
 </script>
 
 <UserWidget />
+
+<button
+	class="btn"
+	on:click={() => {
+		fetch('/api/download/rename', {
+			method: 'POST',
+			body: JSON.stringify({
+				filename: 'thumbnail-shrimp.png',
+				renamed: 'bababooey.png'
+			})
+		})
+			.then(res => {
+				console.log(res);
+				return res;
+			})
+			.then(res => res.blob())
+			.then(blob => URL.createObjectURL(blob))
+			.then(href => {
+				const a = document.createElement('a');
+				document.body.appendChild(a);
+				a.style = 'display: none';
+				a.href = href;
+				a.download = 'bazinga.png';
+				a.click();
+			})
+			.catch(console.error);
+	}}>Download test file</button
+>
