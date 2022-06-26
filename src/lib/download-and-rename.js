9295b0b4f6e7eb2f65c700c868df710f852b5c98
joseph-bravo <josephangelobravo@gmail.com>
2022-06-26 06:23:13 -0700

    no need for rename endpoint, handle in client

---
+++ src/lib/download-and-rename.js
@@ -0,0 +1,16 @@
+// @ts-nocheck
+import supabase from './supabase-client';
+
+export default function downloadRename(bucket, filename, newname) {
+	const { publicURL, error } = supabase.storage.from(bucket).getPublicUrl(filename);
+	if (error) console.error(error);
+	fetch(publicURL)
+		.then(res => res.blob())
+		.then(blob => URL.createObjectURL(blob))
+		.then(href => {
+			const a = document.createElement('a');
+			a.href = href;
+			a.download = newname;
+			a.click();
+		});
+}
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,7 @@
 	import supabase from '$lib/supabase-client';
 	import user from '$stores/userSession';
 	import UserWidget from '$components/UserWidget.svelte';
+	import downloadRename from '$lib/download-and-rename';
 
 	supabase.auth.onAuthStateChange((event, session) => {
 		console.log('Auth State has Changed:', event, session);
@@ -38,4 +39,11 @@
 			})
 			.catch(console.error);
 	}}>Download test file</button
+>
+
+<button
+	class="btn"
+	on:click={() => {
+		downloadRename('symbols', 'thumbnail-shrimp.png', 'bazingabruh.png');
+	}}>Download but epic</button
 >
