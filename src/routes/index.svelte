d97777cec3c914e9ac226cd23b5ca86b1225b780
joseph-bravo <josephangelobravo@gmail.com>
2022-06-26 06:35:49 -0700

    remove the other test download button

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -16,34 +16,6 @@
 <button
 	class="btn"
 	on:click={() => {
-		fetch('/api/download/rename', {
-			method: 'POST',
-			body: JSON.stringify({
-				filename: 'thumbnail-shrimp.png',
-				renamed: 'bababooey.png'
-			})
-		})
-			.then(res => {
-				console.log(res);
-				return res;
-			})
-			.then(res => res.blob())
-			.then(blob => URL.createObjectURL(blob))
-			.then(href => {
-				const a = document.createElement('a');
-				document.body.appendChild(a);
-				a.style = 'display: none';
-				a.href = href;
-				a.download = 'bazinga.png';
-				a.click();
-			})
-			.catch(console.error);
-	}}>Download test file</button
->
-
-<button
-	class="btn"
-	on:click={() => {
 		downloadRename('symbols', 'thumbnail-shrimp.png', 'bazingabruh.png');
 	}}>Download but epic</button
 >
