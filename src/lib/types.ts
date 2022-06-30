1480c9fca04d61d87ff374e4050c37d018bc6a5f
Joseph Bravo <josephangelobravo@gmail.com>
2022-06-30 12:41:13 -0700

    starting work on frontend created type for Post intellisense look into color for BAKA post

---
+++ src/lib/types.ts
@@ -0,0 +1,10 @@
+export interface Post {
+	created_at: string;
+	picture: string;
+	post_id: number;
+	sar_filename: string;
+	thumbnail_filename: string;
+	title: string;
+	user_id: string;
+	username: string;
+}
--- src/routes/catalog/[page].svelte
+++ src/routes/catalog/[page].svelte
@@ -1,9 +1,10 @@
 <script>
 	import supabase from '$lib/supabase-client';
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 
 	onMount(async () => {
-		const { body, error } = await supabase.rpc('posts_meta', { page: 0 });
+		const { body, error } = await supabase.rpc('posts_meta', { page: $page.params.page });
 		if (error) console.error(error);
 		const bodyParsed = body.map(e => {
 			e.thumbnail = supabase.storage.from('symbols').getPublicUrl(e.thumbnail_filename).publicURL;
@@ -17,11 +18,11 @@
 	$: console.log(posts);
 </script>
 
-<div class="max-w-4xl mx-auto ">
-	<ul class="grid md:grid-cols-2 gap-4 p-4">
+<div class="max-w-3xl mx-auto">
+	<ul class="grid md:grid-cols-2">
 		{#each posts as post}
-			<li class="rounded-box bg-base-100 grid grid-cols-6 gap-2 p-2">
-				<img class="col-span-2 rounded-box" src={post.thumbnail} alt="" />
+			<li class="rounded-box bg-base-100 grid grid-cols-6">
+				<img class="col-span-2" src={post.thumbnail} alt="" />
 				<h3 class="col-span-4">{post.title}</h3>
 			</li>
 		{/each}
