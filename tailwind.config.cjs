36ddea9fbca3c92d2169d46ec87e6152038e19aa
Joseph Bravo <arks.josephbravo@gmail.com>
2022-10-04 16:05:16 -0700

    init sveltekit tailwind and daisyui

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+const config = {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
+
+	daisyui: {
+		themes: ['dark']
+	}
+};
+
+module.exports = config;
